4bfa0c4e4b9be2562dbadbd375682b30ae3e870f
yahavz <yhv2000z@gmail.com>
2020-03-19 18:57:28 +0200

    protocol.h: Defined constants to ack packets types, added packet_headers struct as the first member of every other packet, changed MAX_FRAGMENT_AT_ONCE to MAX_CHUNKS

--- Protocol/protocol.h
+++ Protocol/protocol.h
@@ -5,22 +5,24 @@
 
 #define TYPE_SYN 0x1
 #define TYPE_ACK 0x2
+#define TYPE_SYNACK (TYPE_SYN| TYPE_ACK)
 #define TYPE_PSH 0x4
 #define TYPE_EOP 0x8
+#define TYPE_EOPACK (TYPE_EOP| TYPE_ACK)
 #define TYPE_FIN 0x10
 
-#define MAX_FRAGMENT_AT_ONCE (128)
+#define MAX_CHUNKS (128)
 #define MAX_PHASE_INDEX ((MAXDWORD / 128) - 1)
-#define ACK_BITFIELD_SIZE (MAX_FRAGMENT_AT_ONCE / 8)
+#define ACK_BITFIELD_SIZE (MAX_CHUNKS / 8)
 #define CRC_SIZE (sizeof(((p_packet_headers)0)->crc))
 
 #define PACKET_HEADERS_SIZE (sizeof(packet_headers))
-#define SYN_PACKET_SIZE (PACKET_HEADERS_SIZE + sizeof(syn_packet))
-#define SYNACK_PACKET_SIZE (PACKET_HEADERS_SIZE)
-#define PSH_PACKET_SIZE (PACKET_HEADERS_SIZE + sizeof(psh_packet))
-#define EOP_PACKET_SIZE (PACKET_HEADERS_SIZE + sizeof(eop_packet))
-#define EOPACK_PACKET_SIZE (PACKET_HEADERS_SIZE + sizeof(eopack_packet))
-#define FIN_PACKET_SIZE (PACKET_HEADERS_SIZE)
+#define SYN_PACKET_SIZE (sizeof(syn_packet))
+#define SYNACK_PACKET_SIZE (sizeof(synack_packet))
+#define PSH_PACKET_SIZE (sizeof(psh_packet))
+#define EOP_PACKET_SIZE (sizeof(eop_packet))
+#define EOPACK_PACKET_SIZE (sizeof(eopack_packet))
+#define FIN_PACKET_SIZE (sizeof(fin_packet))
 
 #pragma pack(1)
 
@@ -30,10 +32,16 @@ typedef struct {
 }packet_headers, *p_packet_headers;
 
 typedef struct {
+	packet_headers headers;
 	DWORD numberOfChunks;
 } syn_packet, *p_syn_packet;
 
 typedef struct {
+	packet_headers headers;
+} synack_packet, *p_synack_packet;
+
+typedef struct {
+	packet_headers headers;
 	USHORT fragSize;
 	DWORD fragPhase;
 	BYTE fragIndex;
@@ -43,13 +51,19 @@ typedef struct {
 } psh_packet, *p_psh_packet;
 
 typedef struct {
+	packet_headers headers;
 	DWORD fragPhase;
 } eop_packet, *p_eop_packet;
 
 typedef struct {
+	packet_headers headers;
 	DWORD ackPhase;
 	BYTE ackField[ACK_BITFIELD_SIZE];
 } eopack_packet, *p_eopack_packet;
+
+typedef struct {
+	packet_headers headers;
+} fin_packet, *p_fin_packet;
 
 #pragma pack()
 
